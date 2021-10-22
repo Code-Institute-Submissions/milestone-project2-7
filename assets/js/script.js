98c7d143003d4f9b34c5a0ab01923575ab9b4943
rodolazo <rodolfo.lazo@yahoo.com>
2021-10-22 03:21:45 +0000

    Add code to the begin method

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
 let game = {
-    //Initialize the game
+    /**
+     * Method: Initialize the game
+     */
     begin: function() {
+    
+        //Property to know the location of the question
+        this.currLocation = 0;
+        //Property to know the actual score
+        this.score = 0; 
+    
+        // get the array of <li> elements
+        let arrLi = document.querySelectorAll('.options');    
+              
+        //Iterate all the <li> elements ,add eventListener and check answer
+        arrLi.forEach((element, i) => {
+                
+          element.addEventListener('click', () => {
+            // check correct answer
+            this.checkAnswer(i);
+          });
+        });
+        
+        // Update the score
+        this.updateScore();
+        
+        // show first question
+        this.showQuery(questions[this.currLocation]);
     },
     
     //Show the spanish word and all the alternatives in english
