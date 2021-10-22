af152876e6377688bd783dcaa60b0c31c43844fa
rodolazo <rodolfo.lazo@yahoo.com>
2021-10-22 03:18:40 +0000

    Create the main object and all its methods

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let game = {
+    //Initialize the game
+    begin: function() {
+    },
+    
+    //Show the spanish word and all the alternatives in english
+    showQuery: function(q) {
+    },
+    
+    //Verify the option selected by the user
+    checkAnswer: function() {
+    },
+    
+    //Update position of the array
+    increasePos: function() {      
+    },
+    
+    //Print score on the board game
+    updateScore: function() {
+    },
+    
+    //Print result of selection on the board game
+    showResult: function() {      
+    }
+    
+  };
+  
+  // begin the application
+  game.begin();
+  
